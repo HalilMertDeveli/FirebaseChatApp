b07fb85dcf5e06c85b30d67592eb27a347390004
haliko <halilmertdeveliii@gmail.com>
2023-06-20 11:08:30 +0300

    we got controller.dart

---
+++ lib/common/routes/pages.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_firebase_chat_app/common/routes/names.dart';
+
+class AppPages {
+  static const INITIAL = AppRoutes.INITIAL;
+  static const APPlication = AppRoutes.Application;
+  
+}
