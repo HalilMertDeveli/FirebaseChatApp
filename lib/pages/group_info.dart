8de9d3888b7aff63ae33e513bfecebb3923ceb29
haliko <halilmertdeveliii@gmail.com>
2023-08-12 16:19:33 +0300

    i finished the group info part

--- lib/pages/group_info.dart
+++ lib/pages/group_info.dart
@@ -40,6 +40,9 @@ class _GroupInfoState extends State<GroupInfo> {
   String getName(String r) {
     return r.substring(r.indexOf("_") + 1);
   }
+  String getId(String res){
+    return res.substring(0,res.indexOf("_"));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -115,48 +118,52 @@ class _GroupInfoState extends State<GroupInfo> {
     return StreamBuilder(
       stream: members,
       builder: (context, AsyncSnapshot snapshot) {
-
         if (snapshot.hasData) {
-
           if (snapshot.data['members'] != null) {
-            if(snapshot.data["members"].length != 0){
+            if (snapshot.data["members"].length != 0) {
               return ListView.builder(
                 itemCount: snapshot.data["members"].length,
-                shrinkWrap:true,
-                itemBuilder:(context, index) {
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
                   return Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 5,vertical: 10),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
                     child: ListTile(
                       leading: CircleAvatar(
                         radius: 30,
                         backgroundColor: Theme.of(context).primaryColor,
-                        child: Text(getName(snapshot.data['members'][index]).substring(0,1).toUpperCase(),style: TextStyle(color: Colors.white,fontSize: 15,fontWeight: FontWeight.bold),),
-                      )
+                        child: Text(
+                          getName(snapshot.data['members'][index])
+                              .substring(0, 1)
+                              .toUpperCase(),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      title:  Text(getName(snapshot.data['members'][index])),
+                      subtitle: Text(getId(snapshot.data["members"][index])),
                     ),
                   );
-                    
-              },);
-
+                },
+              );
+            } else {
+              return const Center(
+                child: Text("No members in that group"),
+              );
             }
-            else{
-              return const Center(child:Text("No members in that group"),);
-
-            }
-
-          } 
-
-          else {
-            return const Center(child:Text("No members in that group"),);
+          } else {
+            return const Center(
+              child: Text("No members in that group"),
+            );
           }
-        } 
-        else {
-
+        } else {
           return Center(
             child: CircularProgressIndicator(
               color: Theme.of(context).primaryColor,
             ),
           );
-
         }
       },
     );
