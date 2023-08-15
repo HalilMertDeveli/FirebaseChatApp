946321a156e566df5feb10e50589095755ab4355
haliko <halilmertdeveliii@gmail.com>
2023-08-15 18:44:42 +0300

    search tings

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -15,6 +15,7 @@ class _SearchPageState extends State<SearchPage> {
   bool isLoading = false;
   QuerySnapshot? searchSnapshot;
   bool hasUserSearched = false;
+  bool isJoined = false;
   String userName = "";
   User? user;
 
@@ -25,12 +26,21 @@ class _SearchPageState extends State<SearchPage> {
   }
 
   void getCurrentUserIdAndName() async {
-    await HelperFunctions.getUserEmailFromSF().then((value) {setState(() {
-      userName = value!;
-    });});
+    await HelperFunctions.getUserEmailFromSF().then((value) {
+      setState(() {
+        userName = value!;
+      });
+    });
     user = FirebaseAuth.instance.currentUser;
   }
 
+  String getName(String r) {
+    return r.substring(r.indexOf("_") + 1);
+  }
+
+  String getId(String res) {
+    return res.substring(0, res.indexOf("_"));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,57 +73,133 @@ class _SearchPageState extends State<SearchPage> {
                     style: TextStyle(
                       color: Colors.white,
                     ),
-                    decoration:InputDecoration(
-                      border:InputBorder.none,
-                      hintStyle:TextStyle(color:Colors.white,fontSize:16,),
-                      hintText:"Search groups ...",
-                      
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                      hintText: "Search groups ...",
                     ),
                   ),
                 ),
                 GestureDetector(
-                  onTap:()=>initiateSearchMethot(),
+                  onTap: () => initiateSearchMethot(),
                   child: Container(
-                    width:40,
-                    height:40,
-                    decoration:BoxDecoration(
-                      color:Colors.white.withOpacity(0.1),
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.1),
                       borderRadius: BorderRadius.circular(40),
                     ),
-                    child:const Icon(Icons.search,color:Colors.white,),
+                    child: const Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ],
             ),
           ),
-          isLoading ?  Center(child:CircularProgressIndicator(color:Theme.of(context).primaryColor),) : groupList(),
+          isLoading
+              ? Center(
+                  child: CircularProgressIndicator(
+                      color: Theme.of(context).primaryColor),
+                )
+              : groupList(),
         ],
       ),
     );
   }
-  initiateSearchMethot()async{
-    if(searchController.text.isNotEmpty){
+
+  initiateSearchMethot() async {
+    if (searchController.text.isNotEmpty) {
       setState(() {
         isLoading = true;
-
       });
-      await DatabaseService().searchByName(searchController.text).then((snapshot){
+      await DatabaseService()
+          .searchByName(searchController.text)
+          .then((snapshot) {
         setState(() {
           searchSnapshot = snapshot;
-          isLoading=false;
+          isLoading = false;
           hasUserSearched = true;
         });
       });
     }
   }
-  groupList(){
-    return hasUserSearched ? ListView.builder(itemBuilder: (BuildContext context,int index)=>groupTile(userName,searchSnapshot!.docs[index]["groupId"],searchSnapshot!.docs[index]["groupName"],searchSnapshot!.docs[index]["admin"]),shrinkWrap:true,itemCount:searchSnapshot!.docs.length,): Container();
+
+  groupList() {
+    return hasUserSearched
+        ? ListView.builder(
+            itemBuilder: (BuildContext context, int index) => groupTile(
+                userName,
+                searchSnapshot!.docs[index]["groupId"],
+                searchSnapshot!.docs[index]["groupName"],
+                searchSnapshot!.docs[index]["admin"]),
+            shrinkWrap: true,
+            itemCount: searchSnapshot!.docs.length,
+          )
+        : Container();
   }
-  
-  Widget groupTile(String userName,String groupId,String groupName,String admin) {
+
+  Widget groupTile(
+      String userName, String groupId, String groupName, String admin) {
     //function -> check whether user alredy has 'a group
-    
-    return Text("Hello world");
+    joinedOrNot(userName, groupId, groupName, admin);
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      leading: CircleAvatar(
+        radius: 30,
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Text(
+          groupName.substring(0, 1).toUpperCase(),
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      title: Text(
+        groupName,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+      subtitle: Text("Admin :${getName(admin)}"),
+      trailing: InkWell(
+        onTap: () async {},
+        child: isJoined
+            ? Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black,
+                  border: Border.all(color: Colors.white, width: 1),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: const Text(
+                  "Joined",
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            : Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Theme.of(context).primaryColor,
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                child: const Text("Join",style: TextStyle(color: Colors.white),),
+              ),
+      ),
+    );
   }
-  
+
+  void joinedOrNot(
+      String userName, String groupId, String groupName, String admin) async {
+    await DatabaseService(uid: user!.uid)
+        .isUserJoined(groupName, groupId, userName)
+        .then((value) {
+      setState(() {
+        isJoined = value;
+      });
+    });
+  }
 }
